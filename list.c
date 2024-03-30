7c1ceac4425e33afe81200abcb0559e1bf293336
Your Name <you@example.com>
2024-03-30 18:15:59 +0000

    new try

--- list.c
+++ list.c
@@ -104,9 +104,9 @@ void pushCurrent(List * list, void * data)
 
   if (list->current == list->tail && list->current != NULL)
   {
-    L->next = list->current->next;
-    L->prev = list->current;
-    list->current->next = L;
+    L->next = list->tail->next;
+    L->prev = list->tail;
+    list->tail->next = L;
     list->current  = L;
     
   }
